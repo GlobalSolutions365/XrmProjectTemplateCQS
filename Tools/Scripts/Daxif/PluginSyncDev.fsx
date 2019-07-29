70c916ba384dbf23b5e7668c08b30fb350c052d5
Łukasz Grzybowski-Glikman <kowgli@kowgli.pl>
2019-07-29 15:30:05 +0200

    Moved Scripts to Tools subfolder

--- Tools/Scripts/Daxif/PluginSyncDev.fsx
+++ Tools/Scripts/Daxif/PluginSyncDev.fsx
@@ -7,7 +7,7 @@ open _Config
 open DG.Daxif
 open DG.Daxif.Common.Utility
 
-let pluginProjFile = Path.solutionRoot ++ @"Xrm.Plugins\Xrm.Plugins.csproj"
-let pluginDll = Path.solutionRoot ++ @"Xrm.Plugins\bin\Release\ILMerge\Xrm.Plugin.dll"
+let pluginProjFile = Path.solutionRoot ++ @"..\Xrm.Plugins\Xrm.Plugins.csproj"
+let pluginDll = Path.solutionRoot ++ @"..\Xrm.Plugins\bin\Release\ILMerge\Xrm.Plugin.dll"
 
 Plugin.Sync(Env.dev, pluginDll, pluginProjFile, SolutionInfo.name)
