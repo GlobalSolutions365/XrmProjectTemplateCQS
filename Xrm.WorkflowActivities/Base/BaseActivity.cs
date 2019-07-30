c5b6f8d862f9a93e4a05c86a3fe83ed19ba0f8a9
Łukasz Grzybowski-Glikman <kowgli@kowgli.pl>
2019-07-30 10:38:30 +0200

    Initial version of base workflow activity

---
+++ Xrm.WorkflowActivities/Base/BaseActivity.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System.Activities;
+
+namespace Xrm.WorkflowActivities.Base
+{
+    public class BaseActivity : CodeActivity
+    {
+        public CodeActivityContext CodeActivityContext { get; private set; }
+        public ITracingService TracingService { get; private set; }
+        public IWorkflowContext WorkflowContext { get; private set; }
+        public IOrganizationService OrgService { get; private set; }
+        public IOrganizationService OrgServiceAsSystem { get; private set; }
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            var factory = executionContext.GetExtension<IOrganizationServiceFactory>() as IOrganizationServiceFactory;
+
+            CodeActivityContext = executionContext;
+            TracingService = executionContext.GetExtension<ITracingService>() as ITracingService;
+            WorkflowContext = executionContext.GetExtension<IWorkflowContext>() as IWorkflowContext;
+            OrgService = factory.CreateOrganizationService(WorkflowContext.UserId) as IOrganizationService;
+            OrgServiceAsSystem = factory.CreateOrganizationService(null) as IOrganizationService;
+
+            InternalExecute();
+        }
+
+        protected virtual void InternalExecute() { }
+    }
+}
